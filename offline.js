f7fd56ef78510d02d2042296c9509ecd9fa76a91
Gl0r1ss <sopkanetwork@gmail.com>
2024-09-16 21:35:52 +1000

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1726486100,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/case-sheet0.png",
+		"images/slot-sheet0.png",
+		"images/slot-sheet1.png",
+		"images/slot-sheet2.png",
+		"images/weapon-sheet0.png",
+		"images/weapon-sheet1.png",
+		"images/weapon-sheet2.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/go-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/go-new-gambling_cshjkgni.ogg",
+		"media/gambling.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Amazon-Web-App-API.min.js",
+		"Amazon-Web-App-API-tester.min.js",
+		"tizen-iap.js",
+		"cws-buy.js"
+	]
+}
